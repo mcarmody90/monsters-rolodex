65780b62b910820dca4e19f3a4b94ca10dbe9b9e
Matt Carmody <matt.carmody@ovsgroup.com>
2020-03-11 00:20:54 -0400

    Updates

---
+++ precache-manifest.c80f4764302f258260a8c7c4d9c92487.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650a39e891ffadb75852c187e2ead61a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c99863bf4c214c043147",
+    "url": "/monsters-rolodex/static/css/main.ec0578a9.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c99863bf4c214c043147",
+    "url": "/monsters-rolodex/static/js/main.8336e0b3.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
